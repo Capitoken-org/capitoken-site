1bcac79557954c4bfb83191cf31b67e15a0fd127
Capitoken.org <capi.cash.2024@gmail.com>
2026-01-15 23:34:04 +0000

    Cambio Astro path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig(({ command }) => ({
   site: "https://capitoken-org.github.io",
   base: command === "build" ? "/capitoken-site/" : "/",
+  trailingSlash: "always",
   build: {
     assets: "_astro",
   },
