9781da756772a08664c6005feb26f4b39e1d862e
Capitoken.org <capi.cash.2024@gmail.com>
2026-01-15 23:17:00 +0000

    Cambios deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
 import { defineConfig } from "astro/config";
-import path from "node:path";
 
 export default defineConfig(({ command }) => ({
   site: "https://capitoken-org.github.io",
-  // En DEV (Codespaces) debe ser "/" para que NO pida /capitoken-site y te dé 404.
-  // En BUILD (GitHub Pages) sí debe ser "/capitoken-site".
-  base: command === "build" ? "/capitoken-site" : "/",
-
-  vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve("./src"),
-      },
-    },
-  },
+  base: command === "build" ? "/capitoken-site/" : "/",
 }));
