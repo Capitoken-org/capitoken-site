ad2bea349348c7e32037ecb2b69201f77deed2db
Capitoken.org <capi.cash.2024@gmail.com>
2026-01-15 19:53:28 +0000

    Cambios Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
-const isProd = process.env.NODE_ENV === "production";
-
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   site: "https://capitoken-org.github.io",
-  base: isProd ? "/capitoken-site" : "/",
-});
+  base: command === "build" ? "/capitoken-site/" : "/",
+}));
