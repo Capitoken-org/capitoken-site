f640ed71b9114b69fb34ce623705fe7b383f1cbf
Capitoken.org <capi.cash.2024@gmail.com>
2026-01-16 09:12:38 +0000

    astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // GitHub Pages project site: https://capitoken-org.github.io/capitoken-site/
+  site: 'https://capitoken-org.github.io',
+  base: '/capitoken-site',
+  output: 'static',
+});
